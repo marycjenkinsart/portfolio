b1890c3bad0ac44c6360601e6e40a53d04610b83
Mary C Jenkins <marycjenkinsart@gmail.com>
2025-01-02 15:20:12 -0700

    In progress "process viewer"

--- slider.js
+++ slider.js
@@ -1,5 +1,4 @@
 const makeSlider = (selector) => {
-	// const hero = document.querySelector(selector);
 	const slider = document.querySelector(selector + ' .slider');
 	const left = document.querySelector(selector + ' .left');
 	const right = document.querySelector(selector + ' .right');
@@ -21,4 +20,28 @@ const makeSlider = (selector) => {
 		sliderPosition %= sliderItemCount;
 		slider.style.left = -100*sliderPosition + '%';
 	});
+};
+
+const makeProcess = (selector) => {
+	const sequence = document.querySelector(selector + ' .sequence');
+	const left = document.querySelector(selector + ' .left');
+	const right = document.querySelector(selector + ' .right');
+
+	const sequenceItemCount = sequence.children.length;
+	const sequenceItems = [...sequence.children];
+	let sequencePosition = 0;
+	left.addEventListener('click', () => {
+		sequenceItems[sequencePosition].removeAttribute('style');
+		sequencePosition += sequenceItemCount;
+		sequencePosition -= 1;
+		sequencePosition %= sequenceItemCount;
+		sequenceItems[sequencePosition].style = 'z-index: 100;'
+	});
+	right.addEventListener('click', () => {
+		sequenceItems[sequencePosition].removeAttribute('style');
+		sequencePosition += 1;
+		sequencePosition %= sequenceItemCount;
+		sequenceItems[sequencePosition].style = 'z-index: 100;'
+	});
+
 };
