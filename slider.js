5180d7ac1048f051c0462806a7326fd7513fc255
Mary C Jenkins <marycjenkinsart@gmail.com>
2025-01-02 14:47:49 -0700

    Slider is reusable

--- slider.js
+++ slider.js
@@ -1,19 +1,24 @@
-const hero = document.getElementById('top');
-const slider = document.querySelector('#top .slider');
-const left = document.querySelector('#top .left');
-const right = document.querySelector('#top .right');
+const makeSlider = (selector) => {
+	// const hero = document.querySelector(selector);
+	const slider = document.querySelector(selector + ' .slider');
+	const left = document.querySelector(selector + ' .left');
+	const right = document.querySelector(selector + ' .right');
 
-const sliderItemCount = slider.children.length;
-let sliderPosition = 0;
-left.addEventListener('click', () => {
-    sliderPosition += sliderItemCount;
-    sliderPosition -= 1;
-    sliderPosition %= sliderItemCount;
-    slider.style.left = -100*sliderPosition + '%';
-});
-right.addEventListener('click', () => {
-    sliderPosition += 1;
-    sliderPosition %= sliderItemCount;
-    slider.style.left = -100*sliderPosition + '%';
-});
-
+	const sliderItemCount = slider.children.length;
+	slider.style.width = sliderItemCount * 100 + '%';
+	[...slider.children].forEach((imageContainer)=>{
+		imageContainer.style.width = `calc(100% / ${sliderItemCount})`;
+	})
+	let sliderPosition = 0;
+	left.addEventListener('click', () => {
+		sliderPosition += sliderItemCount;
+		sliderPosition -= 1;
+		sliderPosition %= sliderItemCount;
+		slider.style.left = -100*sliderPosition + '%';
+	});
+	right.addEventListener('click', () => {
+		sliderPosition += 1;
+		sliderPosition %= sliderItemCount;
+		slider.style.left = -100*sliderPosition + '%';
+	});
+};
